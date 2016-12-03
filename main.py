eb86754d747277d4a820116fa033adaf3f1d174c
14mthalvorson <14mthalvorson@gmail.com>
2016-12-03 12:35:04 -0600

    Added timer stats for length of simulation runtime, average game, exceptions met.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import Simulator
 import time
-from threading import Thread
+import threading
+
+
+# Parts of myThread copied from Tutorials Point page on Multithreading
+class Thread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        sim = Simulator.Simulator([
+            {"name": "Alvin"},
+            {"name": "Brady"},
+            {"name": "Charlie"},
+            {"name": "Donnie"},
+            {"name": "Ernie"}
+        ], False)
 
 # Keeps track of total wins by each player
 player_wins = {}
@@ -11,9 +26,13 @@ power_wins = {}
 # Keeps track of total games played by each power
 power_count = {}
 
-num_games_simulated = 1000
+num_games_simulated = 10000
 
 num_of_threads = 5
+
+start_time = time.clock()
+
+exceptions = 0
 
 for i in range(num_games_simulated):
 
@@ -26,6 +45,7 @@ for i in range(num_games_simulated):
             {"name": "Donnie"},
             {"name": "Ernie"}
         ], True)
+
     if False:  # Debugging game mode
         sim = Simulator.Simulator([
             {"name": "Alvin", "power": "Parasite"},
@@ -34,11 +54,8 @@ for i in range(num_games_simulated):
             {"name": "Donnie", "power": "Tripler"},
             {"name": "Ernie", "power": "Symbiote"}
             ], True)
+
     if True:  # Simulation mode
-
-
-
-
         try:
             sim = Simulator.Simulator([
                 {"name": "Alvin"},
@@ -48,10 +65,11 @@ for i in range(num_games_simulated):
                 {"name": "Ernie"}
             ], False)
         except:
+            exceptions += 1
             i -= 1
-
         if i % 200 == 0:
             print(i)
+
     if False:  # Debugging Simulation mode
         sim = Simulator.Simulator([
                 {"name": "Alvin"},
@@ -70,8 +88,14 @@ for i in range(num_games_simulated):
             power_wins[player.power] = power_wins.get(player.power, 0) + 1
         power_count[player.power] = power_count.get(player.power, 0) + 1
 
+# Display clock stats
+end_time = time.clock()
+print("\nTotal Time:\n" + str(round(end_time - start_time, 2)) + " seconds")
+print("Average Time for a Simulated Game:\n" + str(round((end_time - start_time) / num_games_simulated, 4)) + " seconds")
+print("Number of Exceptions: " + str(exceptions))
+
 # Display winning percentages for each player
-print("Player Win Percentages:")
+print("\nPlayer Win Percentages:")
 player_win_list = [(player.name, player_wins.get(player.name, 0) / num_games_simulated) for player in sim.game.players]
 # Sort so winningest players are first
 player_win_list.sort(key=lambda x: x[1], reverse=True)
